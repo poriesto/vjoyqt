d7685158727a6ae18f07874f505d3efa3ccf845e
Evgeny Proydakov <lord.tiran@gmail.com>
2011-07-30 21:38:29 +0400

    Added VJoystickAdapter files

---
+++ v_joystick_adapter.cpp
@@ -0,0 +1,130 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2011 Evgeny Proydakov lord.tiran@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <SDL/SDL.h>
+
+#include "v_joystick_adapter.h"
+
+VJoystickAdapter::VJoystickAdapter(QObject *parent) :
+    QObject(parent),
+    m_joystick(0)
+{
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
+    SDL_JoystickEventState(SDL_ENABLE);
+    m_joystickThread = new VJoystickThread(this);
+}
+
+VJoystickAdapter::~VJoystickAdapter()
+{
+    if(isConnected())
+        close();
+    delete m_joystickThread;
+    SDL_JoystickEventState(SDL_DISABLE);
+    SDL_Quit();
+}
+
+bool VJoystickAdapter::open(int id)
+{
+    Q_ASSERT(!m_joystick);
+
+    if(SDL_JoystickOpened(id))
+        return false;
+    m_joystick = SDL_JoystickOpen(id);
+    if(SDL_JoystickOpened(id))
+    {
+        m_joystickThread->start();
+    }
+    else
+        m_joystick = 0;
+    return m_joystick;
+}
+
+void VJoystickAdapter::close()
+{
+    if(m_joystick)
+    {
+        SDL_Event closeEvent;
+
+        closeEvent.type = SDL_QUIT;
+        SDL_PushEvent(&closeEvent);
+
+        m_joystickThread->wait();
+        SDL_JoystickClose(m_joystick);
+        m_joystick = 0;
+    }
+}
+
+void VJoystickAdapter::VJoystickThread::run()
+{
+    SDL_Event joyEvent;
+    bool running = true;
+
+    while(running)
+    {
+        SDL_WaitEvent(&joyEvent);
+
+        if(joyEvent.type == SDL_QUIT)
+            running = false;
+        else if(joyEvent.jbutton.which == m_adapter->getJoystickId())
+        {
+            switch(joyEvent.type)
+            {
+            case SDL_JOYAXISMOTION:
+                emit m_adapter->sigAxisChanged(joyEvent.jaxis.axis,joyEvent.jaxis.value);
+                break;
+
+            case SDL_JOYHATMOTION:
+                emit m_adapter->sigHatCanged(joyEvent.jhat.hat, joyEvent.jhat.value);
+                break;
+
+            case SDL_JOYBALLMOTION:
+                emit m_adapter->sigBallChanged(joyEvent.jball.ball, joyEvent.jball.xrel, joyEvent.jball.yrel);
+                break;
+
+            case SDL_JOYBUTTONDOWN:
+            case SDL_JOYBUTTONUP:
+                emit m_adapter->sigButtonChanged(joyEvent.jbutton.button, joyEvent.jbutton.state);
+                break;
+            }
+        }
+    }
+}
+
+int VJoystickAdapter::getNumAvaliableJoystick()
+{
+    SDL_JoystickUpdate();
+    return SDL_NumJoysticks();
+}
+
+QStringList VJoystickAdapter::getAvaliableJoystickName()
+{
+    QStringList joyNames;
+
+    int joyNum = getNumAvaliableJoystick();
+
+    for(int i = 0; i < joyNum; ++i)
+        joyNames.push_front(QString(SDL_JoystickName(i)));
+
+    return joyNames;
+}
---
+++ v_joystick_adapter.h
@@ -0,0 +1,118 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2011 Evgeny Proydakov lord.tiran@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef __V_JOYSTICK_ADAPTER_H
+#define __V_JOYSTICK_ADAPTER_H
+
+#include <QThread>
+#include <QString>
+#include <QStringList>
+#include <SDL/SDL_joystick.h>
+
+class VJoystickAdapter : public QObject
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(VJoystickAdapter)
+
+public:
+    enum HatPosition
+    {
+        JOYSTICK_HAT_CENTERED = SDL_HAT_DOWN,
+        JOYSTICK_HAT_UP = SDL_HAT_UP,
+        JOYSTICK_HAT_UP_RIGHT = SDL_HAT_RIGHT,
+        JOYSTICK_HAT_RIGHT = SDL_HAT_RIGHT,
+        JOYSTICK_HAT_RIGHT_DOWN = SDL_HAT_RIGHTDOWN,
+        JOYSTICK_HAT_DOWN = SDL_HAT_DOWN,
+        JOYSTICK_HAT_DOWN_LEFT = SDL_HAT_LEFTDOWN,
+        JOYSTICK_HAT_LEFT = SDL_HAT_LEFT,
+        JOYSTICK_HAT_LEFT_UP = SDL_HAT_LEFTUP
+    };
+
+public:
+    explicit VJoystickAdapter(QObject *parent = 0);
+    ~VJoystickAdapter();
+
+    bool open(int id);
+    void close();
+    bool isConnected() const
+    {
+        return m_joystick ? SDL_JoystickOpened(getJoystickId()) : false;
+    }
+
+    inline int getJoystickId() const
+    {
+        return SDL_JoystickIndex(m_joystick);
+    }
+    inline QString getJoystickName() const
+    {
+        return QString(SDL_JoystickName(getJoystickId()));
+    }
+    inline int getJoystickNumAxes() const
+    {
+        return SDL_JoystickNumAxes(m_joystick);
+    }
+    inline int getJoystickNumHats() const
+    {
+        return SDL_JoystickNumHats(m_joystick);
+    }
+    inline int getJoystickNumBalls() const
+    {
+        return SDL_JoystickNumBalls(m_joystick);
+    }
+    inline int getJoystickNumButtons() const
+    {
+        return SDL_JoystickNumButtons(m_joystick);
+    }
+
+    static int getNumAvaliableJoystick();
+    static QStringList getAvaliableJoystickName();
+
+signals:
+    void sigButtonChanged(int id, bool state);
+    void sigAxisChanged(int id, int state);
+    void sigHatCanged(int id, int state);
+    void sigBallChanged(int id, int stateX, int stateY);
+
+private:
+    class               VJoystickThread;
+
+    SDL_Joystick*       m_joystick;
+    VJoystickThread*    m_joystickThread;
+};
+
+class VJoystickAdapter::VJoystickThread : public QThread
+{
+    Q_OBJECT
+
+public:
+    inline VJoystickThread(VJoystickAdapter* adapter) : m_adapter(adapter) { }
+
+protected:
+    virtual void run();
+
+private:
+    VJoystickAdapter *m_adapter;
+};
+
+#endif // __V_JOYSTICK_ADAPTER_H
